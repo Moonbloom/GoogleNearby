5579ed4dde4a396e2ee4d74dae94e0290b79a532
Kasper Liholm <khl@trifork.com>
2017-01-18 10:48:17 +0100

    cooode

--- app/src/main/java/nearby/google/trifork/googlenearby/MainActivity.java
+++ app/src/main/java/nearby/google/trifork/googlenearby/MainActivity.java
@@ -1,13 +1,103 @@
 package nearby.google.trifork.googlenearby;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+
+    private GoogleApiClient mGoogleApiClient;
+    private Message mActiveMessage;
+    private MessageListener mMessageListener;
+    private SubscribeOptions options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        options = new SubscribeOptions.Builder()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
+
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                String messageAsString = new String(message.getContent());
+                Log.d(TAG, "Found message: " + messageAsString);
+            }
+
+            @Override
+            public void onLost(Message message) {
+                String messageAsString = new String(message.getContent());
+                Log.d(TAG, "Lost sight of message: " + messageAsString);
+            }
+        };
+    }
+
+    @Override
+    public void onStop() {
+        unpublish();
+        unsubscribe();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        publish("Hello World");
+        subscribe();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void publish(String message) {
+        Log.i(TAG, "Publishing message: " + message);
+        mActiveMessage = new Message(message.getBytes());
+        Nearby.Messages.publish(mGoogleApiClient, mActiveMessage);
+    }
+
+    private void unpublish() {
+        Log.i(TAG, "Unpublishing.");
+        if (mActiveMessage != null) {
+            Nearby.Messages.unpublish(mGoogleApiClient, mActiveMessage);
+            mActiveMessage = null;
+        }
+    }
+
+    private void subscribe() {
+        Log.i(TAG, "Subscribing.");
+        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, options);
+    }
+
+    private void unsubscribe() {
+        Log.i(TAG, "Unsubscribing.");
+        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
     }
 }
