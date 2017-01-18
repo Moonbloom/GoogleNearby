5399707d51f9ae93c9aada3f98fb569cd490e44a
Kasper Liholm <khl@trifork.com>
2017-01-18 11:41:03 +0100

    more

--- app/src/main/java/nearby/google/trifork/googlenearby/MainActivity.java
+++ app/src/main/java/nearby/google/trifork/googlenearby/MainActivity.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.messages.Message;
 import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.Strategy;
 import com.google.android.gms.nearby.messages.SubscribeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -21,12 +32,33 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private MessageListener mMessageListener;
     private SubscribeOptions options;
 
+    private List<Message> activeMessages = new ArrayList<>();
+
+    private NearbyAdapter nearbyAdapter;
+
+    @BindView(R.id.listview)
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        nearbyAdapter = new NearbyAdapter(this, activeMessages);
+        listView.setAdapter(nearbyAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, new String(activeMessages.get(position).getContent()), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Strategy strategy = new Strategy.Builder()
+                .build();
 
         options = new SubscribeOptions.Builder()
+                .setStrategy(strategy)
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -40,12 +72,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             public void onFound(Message message) {
                 String messageAsString = new String(message.getContent());
                 Log.d(TAG, "Found message: " + messageAsString);
+
+                activeMessages.add(message);
+                nearbyAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onLost(Message message) {
                 String messageAsString = new String(message.getContent());
                 Log.d(TAG, "Lost sight of message: " + messageAsString);
+
+                activeMessages.remove(message);
+                nearbyAdapter.notifyDataSetChanged();
             }
         };
     }
@@ -63,18 +101,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        publish("Hello World");
+        publish("KHL");
         subscribe();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.d(TAG, "onConnectionSuspended - " + i);
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.d(TAG, "onConnectionFailed - " + connectionResult);
     }
 
     private void publish(String message) {
---
+++ app/src/main/java/nearby/google/trifork/googlenearby/NearbyAdapter.java
@@ -0,0 +1,50 @@
+package nearby.google.trifork.googlenearby;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.google.android.gms.nearby.messages.Message;
+
+import java.util.List;
+
+public class NearbyAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Message> messages;
+
+    public NearbyAdapter(Context context, List<Message> messages) {
+        this.context = context;
+        this.messages = messages;
+    }
+
+    @Override
+    public int getCount() {
+        return messages != null ? messages.size() : 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return messages != null ? messages.get(position) : null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_nearby, null, false);
+        }
+
+        TextView textView = (TextView) convertView.findViewById(R.id.text);
+        textView.setText(new String(messages.get(position).getContent()));
+
+        return convertView;
+    }
+}
