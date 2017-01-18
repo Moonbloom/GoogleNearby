c6609849f9cb344a1ac1318b244d6225f616a69e
Kasper Liholm <khl@trifork.com>
2017-01-18 10:32:58 +0100

    FIRST!

---
+++ app/src/main/java/nearby/google/trifork/googlenearby/MainActivity.java
@@ -0,0 +1,13 @@
+package nearby.google.trifork.googlenearby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
